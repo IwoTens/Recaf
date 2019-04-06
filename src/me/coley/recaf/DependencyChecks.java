70024cf0b4186b697743a59968688e2d20b77aac
IwoTens <iwailodenisow+iwoTens@gmail.com>
2019-04-06 23:32:26 +0200

    build support for jdk11 12

--- src/me/coley/recaf/DependencyChecks.java
+++ src/me/coley/recaf/DependencyChecks.java
@@ -52,6 +52,8 @@ public class DependencyChecks {
 	public static double getVersion() {
 		String version = System.getProperty("java.version");
 		int pos = version.indexOf('.');
+		if (pos == -1)
+			return Double.parseDouble(version);
 		pos = version.indexOf('.', pos + 1);
 		return Double.parseDouble(version.substring(0, pos));
 	}
